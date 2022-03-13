760bfebc2e70c0fbe073946c446e394096a7cad1
Omar <omaralbahra@gmail.com>
2022-03-13 13:22:53 +0300

    add 422 error validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,18 @@ import { createApp } from "vue"
 import App from './App.vue'
 import Store from './Store'
 import axios from "axios"
+import { createToaster } from "@meforma/vue-toaster";
 
 axios.defaults.baseURL = process.env.MIX_API_BASE_URL;
 
 axios.interceptors.response.use(function (response) {
     return response.data;
 }, function (error) {
+    createToaster().show(error.response.data.message,{
+        type: 'error',
+        position: 'top',
+        message: error.response.data.message,
+    })
     return Promise.reject(error);
 });
 
